cc5efef050342e487332abfa345684d60cee21b7
Evan Anders <evanhanders@gmail.com>
2018-09-17 16:47:47 -0600

    adds first data and first plots

---
+++ plots/fig1.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib
+#matplotlib settings
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+
+fig = plt.figure(figsize=(8.5, 3))
+gs     = gridspec.GridSpec(*(1000,1000))
+gs_info = (((50,0), 950, 250), ((50, 370), 900, 250), ((50, 750), 900, 250))
+ax1 = plt.subplot(gs.new_subplotspec(*gs_info[0]))
+ax2 = plt.subplot(gs.new_subplotspec(*gs_info[1]))
+ax3 = plt.subplot(gs.new_subplotspec(*gs_info[2]))
+
+
+#READ DATA, PLOT 1
+onset_data = np.genfromtxt('../data/eps1e-4_onsets.csv', skip_header=1, delimiter=',', usecols=(0,3))
+ax1.plot(onset_data[:,0], onset_data[:,1], c='k', lw=3)
+
+original_co = np.genfromtxt('../data/coprime_data_original_co_runs.csv', skip_header=1, delimiter=',', usecols=(2,4,6)) #ra, ro, ta -- need to double check if this is backwards ra/ta.
+co1 = original_co[:,0]/original_co[:,2] == 1
+x, y = original_co[co1, 2], original_co[co1, 0]
+ax1.plot(x, y, c='r', ls='--', lw=2, label='Co = 1')
+
+sigma = np.genfromtxt('../data/coprime_data_sigma_runs.csv', skip_header=1, delimiter=',', usecols=(2,4,5,6)) #ra, ro, sigma, ta
+sig1 = sigma[:,2] == 2
+
+ax1.plot(sigma[sig1, 3], sigma[sig1, 0], c='orange', dashes=(4,1,2,1), lw=2, label=r'$\sigma=2$')
+
+new_c = np.genfromtxt('../data/ra_ta0.75_AR4_alldata.csv', skip_header=1, delimiter=',', usecols=(0,1,2,5)) #sigma, ra, ta, ro
+c957 = new_c[:,0] == 0.957
+c158 = new_c[:,0] == 1.58
+#ax1.plot(new_c[c957, 2], new_c[c957, 1])
+ax1.plot(new_c[c158, 2], new_c[c158, 1], c='blue', lw=2, label=r'$\mathcal{C} = 1.58$')
+
+
+ax1.set_xlim(1e1, 1e9)
+ax1.set_ylim(1e1, 1e6)
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.set_xlabel('Ta')
+ax1.set_ylabel('Ra')
+ax1.legend(loc='lower right', frameon=False)
+
+
+#PLOT 2
+
+ax2.plot(new_c[c158, 2], new_c[c158, 3], marker='*', color='blue', lw=0, ms=4, label=r'$\mathcal{C} = 1.58$')
+ax2.plot(new_c[c957, 2], new_c[c957, 3], marker='o', color='blue', lw=0, ms=4, label=r'$\mathcal{C} = 0.96$')
+ax2.plot(sigma[sig1, 3], sigma[sig1, 1], marker='o', color='orange', lw=0, ms=4)
+ax2.plot(original_co[co1, 0], original_co[co1, 1], marker='o', color='red', lw=0, ms=4)
+
+ax2.set_yscale('log')
+ax2.set_xscale('log')
+ax2.set_xlabel('Ra')
+ax2.set_ylabel('Ro')
+
+
+
+# PLOT 3
+
+c_sig = np.sqrt(sigma[sig1, 3]/sigma[sig1, 0]**(3/4))
+ax3.plot(c_sig, sigma[sig1, 1], marker='o', lw=0, c='orange')
+p = np.polyfit(np.log10(c_sig), np.log10(sigma[sig1, 1]), deg=1)
+label_str = 'Ro' + r'$ \propto $'
+str2 = '$\mathcal{C}^{' + '{:.2f}'.format(p[0]) + '}$'
+ax3.plot(c_sig, 10**(p[1])*c_sig**(p[0]), label=label_str+r'{:s}'.format(str2))
+
+ax3.set_yscale('log')
+ax3.set_xscale('log')
+ax3.set_xlabel(r'$\mathcal{C}$')
+ax3.set_ylabel('Ro')
+ax3.legend(loc='upper right', frameon=False)
+
+
+fig.savefig('../tex/figs/parameter_space.png', dpi=300, bbox_inches='tight')
---
+++ plots/fig2.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib
+#matplotlib settings
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+
+
+fig = plt.figure(figsize=(3, 5))
+gs     = gridspec.GridSpec(*(1000,1000))
+gs_info = (((50,50), 400, 900), ((550, 50), 400, 900))
+ax1 = plt.subplot(gs.new_subplotspec(*gs_info[0]))
+ax2 = plt.subplot(gs.new_subplotspec(*gs_info[1]))
+
+
+#READ DATA, PLOT 1
+onset_data = np.genfromtxt('../data/eps1e-4_onsets.csv', skip_header=1, delimiter=',', usecols=(0,3))
+
+original_co = np.genfromtxt('../data/coprime_data_original_co_runs.csv', skip_header=1, delimiter=',', usecols=(2,4,6)) #ra, ro, ta
+co1 = original_co[:,0]/original_co[:,2] == 1
+x, y = original_co[co1, 0], original_co[co1, 2]
+
+sigma = np.genfromtxt('../data/coprime_data_sigma_runs.csv', skip_header=1, delimiter=',', usecols=(2,4,5,6)) #ra, ro, sigma, ta
+sig1 = sigma[:,2] == 2
+
+new_c = np.genfromtxt('../data/ra_ta0.75_AR4_alldata.csv', skip_header=1, delimiter=',') 
+#co, ra, ta, nu, re, ro
+c957 = new_c[:,0] == 0.957
+c158 = new_c[:,0] == 1.58
+lines, labels = [], []
+lines += ax1.plot(new_c[c957, 1], new_c[c957, 3], c='orange', lw=2, label=r'$\mathcal{C} = 0.96$')
+lines += ax1.plot(new_c[c158, 1], new_c[c158, 3], c='blue', lw=2, label=r'$\mathcal{C} = 1.58$')
+
+labels += [r'$\mathcal{C} = 0.96$']
+labels += [r'$\mathcal{C} = 1.58$']
+
+
+p = np.polyfit(np.log10(new_c[c158,1]), np.log10(new_c[c158, 3]), deg=1)
+label_str = 'Nu' + r'$ \propto $'
+str2 = 'Ra$^{' + '{:.2f}'.format(p[0]) + '}$'
+labels += [label_str+r'{:s}'.format(str2)]
+lines += ax1.plot(new_c[c158, 1], 10**(p[1])*new_c[c158, 1]**(p[0]), label=label_str+r'{:s}'.format(str2), color='blue', alpha=0.4)
+p = np.polyfit(np.log10(new_c[c957,1]), np.log10(new_c[c957, 3]), deg=1)
+label_str = 'Nu' + r'$ \propto $'
+str2 = 'Ra$^{' + '{:.2f}'.format(p[0]) + '}$'
+labels += [label_str+r'{:s}'.format(str2)]
+lines += ax1.plot(new_c[c957, 1], 10**(p[1])*new_c[c957, 1]**(p[0]), label=label_str+r'{:s}'.format(str2), color='orange', alpha=0.4)
+
+ax1.legend(lines[:2], labels[:2], loc='upper left', frameon=False)
+from matplotlib.legend import Legend
+leg = Legend(ax1, lines[2:], labels[2:],
+             loc='lower right', frameon=False)
+ax1.add_artist(leg)
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.set_ylabel('Nu')
+
+
+lines, labels = [], []
+#PLOT 2
+lines += ax2.plot(new_c[c957, 1], new_c[c957, 4], c='orange', lw=2, label=r'$\mathcal{C} = 0.96$')
+lines += ax2.plot(new_c[c158, 1], new_c[c158, 4], c='blue', lw=2, label=r'$\mathcal{C} = 1.58$')
+ax2.legend(loc='upper left', frameon=False)
+
+labels += [r'$\mathcal{C} = 0.96$']
+labels += [r'$\mathcal{C} = 1.58$']
+
+p = np.polyfit(np.log10(new_c[c158,1]), np.log10(new_c[c158, 4]), deg=1)
+label_str = 'Re' + r'$ \propto $'
+str2 = 'Ra$^{' + '{:.2f}'.format(p[0]) + '}$'
+labels += [label_str+r'{:s}'.format(str2)]
+lines += ax2.plot(new_c[c158, 1], 10**(p[1])*new_c[c158, 1]**(p[0]), label=label_str+r'{:s}'.format(str2), color='blue', alpha=0.4)
+p = np.polyfit(np.log10(new_c[c957,1]), np.log10(new_c[c957, 4]), deg=1)
+label_str = 'Re' + r'$ \propto $'
+str2 = 'Ra$^{' + '{:.2f}'.format(p[0]) + '}$'
+labels += [label_str+r'{:s}'.format(str2)]
+lines += ax2.plot(new_c[c957, 1], 10**(p[1])*new_c[c957, 1]**(p[0]), label=label_str+r'{:s}'.format(str2), color='orange', alpha=0.4)
+
+ax2.legend(lines[:2], labels[:2], loc='upper left', frameon=False)
+from matplotlib.legend import Legend
+leg = Legend(ax2, lines[2:], labels[2:],
+             loc='lower right', frameon=False)
+ax2.add_artist(leg)
+
+
+
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+ax2.set_xlabel('Ra')
+ax2.set_ylabel('Re')
+
+
+#PLOT 2
+
+fig.savefig('../tex/figs/nu_and_re.png', dpi=300, bbox_inches='tight')
