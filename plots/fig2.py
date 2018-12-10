9affa657c5feba6c1db43f8b335c9da4822dcaa0
Evan Anders <evanhanders@gmail.com>
2018-12-10 16:17:08 -0700

    ready for submission2

--- plots/fig2.py
+++ plots/fig2.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import h5py
 matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['mathtext.fontset'] = 'dejavuserif'
 
 import dedalus.public as de
 
@@ -24,7 +25,7 @@ fig = plt.figure(figsize=(8, 4.5))
 gs     = gridspec.GridSpec(*(1000,1000))
 #gs_info = (((100, 0), 900, 320), ((100, 340), 900, 320), ((100, 680),900, 320))
 gs_info = (((100, 0), 400, 230), ((100, 250), 400, 230), ((100, 500), 400, 230), ((100, 750), 400, 230),
-           ((580, 0), 400, 230), ((580, 250), 400, 230), ((580, 500), 400, 230), ((580, 750), 400, 230))
+           ((510, 0), 400, 230), ((510, 250), 400, 230), ((510, 500), 400, 230), ((510, 750), 400, 230))
 ax1 = plt.subplot(gs.new_subplotspec(*gs_info[0]))
 ax2 = plt.subplot(gs.new_subplotspec(*gs_info[1]))
 ax3 = plt.subplot(gs.new_subplotspec(*gs_info[2]))
@@ -72,10 +73,10 @@ for i, f, ax in zip( range(8), [very_lowF, lowF, highF, coF]*2, [ax4, ax3, ax2, 
     mn *= 0.9
     mx *= 0.9
 
-#    max_abs = np.max([np.abs(mn), np.abs(mx)])
-#    min_abs = np.min([np.abs(mn), np.abs(mx)])
-#    mx = min_abs + (max_abs-min_abs)*0.1
-#    mn = -min_abs - (max_abs - min_abs)*0.1
+    max_abs = np.max([np.abs(mn), np.abs(mx)])
+    min_abs = np.min([np.abs(mn), np.abs(mx)])
+    mx = min_abs + (max_abs-min_abs)*0.25
+    mn = -min_abs - (max_abs - min_abs)*0.25
     
     yy, xx = np.meshgrid(big_y, big_x)
     c = ax.pcolormesh(xx, yy, big_s, cmap='RdBu_r', rasterized=True, vmin=mn, vmax=mx)
@@ -88,27 +89,31 @@ for i, f, ax in zip( range(8), [very_lowF, lowF, highF, coF]*2, [ax4, ax3, ax2, 
 
     mn *= 1e5
     mx *= 1e5
-    ax.annotate(r'$(S_{\mathrm{min}}, S_{\mathrm{max}}) = $' \
-              + r'$({:.1f}, {:.1f})$'.format(mn, mx), 
-              xy=(0.03, -0.09), fontsize=8, annotation_clip=False)
+#    ax.annotate(r'$(S_{\mathrm{min}}, S_{\mathrm{max}}) = $' \
+#              + r'$({:.1f}, {:.1f})$'.format(mn, mx), 
+#              xy=(0.03, -0.09), fontsize=8, annotation_clip=False)
+    bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.75)
+    ax.text(0.77, 0.09, r'$|S| = $' \
+              + r'${:.2f}$'.format(mx), 
+              fontsize=8, ha='center', va='center', bbox=bbox_props)
     count += 1
 
-cax = plt.subplot(gs.new_subplotspec((0, 350), 75, 300))
+cax = plt.subplot(gs.new_subplotspec((35, 350), 43, 300))
 bar = plt.colorbar(c, cax=cax, orientation='horizontal')
 cax.set_xticklabels(())
 bar.set_ticks(())
-cax.annotate(r'$S_{\mathrm{min}} \times 10^{-5}$', fontsize=8,  xy=(-0.35, 0.25), annotation_clip=False)
-cax.annotate(r'$S_{\mathrm{max}} \times 10^{-5}$', fontsize=8,  xy=(1.02, 0.25), annotation_clip=False)
+cax.annotate(r'$-|S| \times 10^{-5}$', fontsize=8,  xy=(-0.37, 0.5), va='center', annotation_clip=False)
+cax.annotate(r'$|S| \times 10^{-5}$', fontsize=8,  xy=(1.02, 0.5),  va='center',  annotation_clip=False)
 
-bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
+bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.75)
 ax4.text(0.26, 0.91, "Ro = 0.026", ha="center", va="center", size=8, bbox=bbox_props)
 ax3.text(0.24, 0.91, "Ro = 0.13", ha="center", va="center", size=8, bbox=bbox_props)
 ax2.text(0.24, 0.91, "Ro = 0.42", ha="center", va="center", size=8, bbox=bbox_props)
 ax1.text(0.24, 0.91, "Ro = 1.21", ha="center", va="center", size=8, bbox=bbox_props)
-ax8.text(0.26, 0.91, "Ro = 0.026", ha="center", va="center", size=8, bbox=bbox_props)
-ax7.text(0.24, 0.91, "Ro = 0.13", ha="center", va="center", size=8, bbox=bbox_props)
-ax6.text(0.24, 0.91, "Ro = 0.42", ha="center", va="center", size=8, bbox=bbox_props)
-ax5.text(0.24, 0.91, "Ro = 1.21", ha="center", va="center", size=8, bbox=bbox_props)
+#ax8.text(0.26, 0.91, "Ro = 0.026", ha="center", va="center", size=8, bbox=bbox_props)
+#ax7.text(0.24, 0.91, "Ro = 0.13", ha="center", va="center", size=8, bbox=bbox_props)
+#ax6.text(0.24, 0.91, "Ro = 0.42", ha="center", va="center", size=8, bbox=bbox_props)
+#ax5.text(0.24, 0.91, "Ro = 1.21", ha="center", va="center", size=8, bbox=bbox_props)
 
 print('saving png')
 fig.savefig('../tex/figs/dynamics_plot.png', dpi=400, bbox_inches='tight')
